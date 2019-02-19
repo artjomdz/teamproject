de7b723b7fe719fd8af89aca8142ebb6ed7faea9
Petr Stoklas <petrstoklas@gmail.com>
2019-02-19 16:18:08 +0100

    show added

--- classified/app/Http/Controllers/QuestionController.php
+++ classified/app/Http/Controllers/QuestionController.php
@@ -9,4 +9,11 @@ class QuestionController extends Controller
     public function index () {
         return 'This is a list of questions';
     }
+
+    public function show ($id)
+    {
+        
+        var_dump($id);
+        return 'This is a detail of a question';
+    }
 }
