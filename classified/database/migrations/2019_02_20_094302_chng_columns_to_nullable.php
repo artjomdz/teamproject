564152cd0397011c681d84c70854dd849eb9d6c2
Petr Stoklas <petrstoklas@gmail.com>
2019-02-20 10:49:38 +0100

    change columns in questions to nullable

---
+++ classified/database/migrations/2019_02_20_094302_chng_columns_to_nullable.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChngColumnsToNullable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->string('title')->nullable()->change();
+            $table->mediumText('text')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->string('title')->nullable(false)->change();
+            $table->mediumText('text')->nullable(false)->change();
+        });
+
+    }
+}
